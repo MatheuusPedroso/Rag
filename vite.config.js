3ce8ff50fd69c109b3ce64db960c16078632a1df
Matheus Pedroso <134893467+MatheuusPedroso@users.noreply.github.com>
2025-04-24 15:06:44 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  // Adicione esta linha:
+  base: '/<NOME_DO_SEU_REPOSITORIO_NO_GITHUB>/', // Substitua pelo nome exato!
   resolve: {
     alias: {
-      // Confirme que esta linha está exatamente assim
       '@': path.resolve(__dirname, './src'),
     },
   },
